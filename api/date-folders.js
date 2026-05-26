ff2129d4bb6ad201655812ea414a7c7bd855cc63
posintra <posintra7@gmail.com>
2026-05-26 13:01:03 +0900

    feat : second commit

---
+++ api/date-folders.js
@@ -0,0 +1,18 @@
+﻿const fs = require("fs");
+const path = require("path");
+
+module.exports = (req, res) => {
+  try {
+    const baseDir = process.cwd();
+    const entries = fs.readdirSync(baseDir, { withFileTypes: true });
+    const dates = entries
+      .filter((entry) => entry.isDirectory() && /^\d{6}$/.test(entry.name))
+      .map((entry) => entry.name)
+      .sort();
+
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.status(200).send({ dates });
+  } catch (error) {
+    res.status(500).send({ dates: [], error: "failed_to_list_date_folders" });
+  }
+};
